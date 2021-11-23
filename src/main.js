03893dfb68499abf9822045f8d4969dfaa5c6834
Ryosuke.o <gogor35@yahoo.co.jp>
2021-11-23 15:59:06 +0900

    boostrap 導入

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css' // boostrap
+import 'bootstrap-vue/dist/bootstrap-vue.css' // boostrap
 
 Vue.config.productionTip = false
 
